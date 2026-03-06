57eed8f4d4b47ab0963a2c2a4dd87e1237c999e0
root <root@ip-172-31-38-34.ap-south-1.compute.internal>
2026-03-06 13:18:59 +0000

    2nd update

--- devops-app/index.js
+++ devops-app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("updated DevOps CI/CD pipeline working 🚀");
+  res.send("updated DevOps CI/CD pipeline 🚀");
 });
 
 app.listen(3000, () => {
